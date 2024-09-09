31d1e62adff053e545462137a2b7bb9cc0a5f014
fudarts <federicofiore01@gmail.com>
2024-09-09 17:03:38 +0200

    Aggiungere i file di progetto.

---
+++ CSharpCorso/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+using CSharpCorso; // Userà le nostre librerie
+// Dichiarazione di variabile di tipo intero + assegnazione
+int a = 5; 
+int c = 1;
+// char d = 'a';
+string nome = "pluto";
+string cognome = "paperino";
+// "b" sarà vero solo se "a" è diverso da 10 OPPURE se "c" è uguale a 5
+bool b = a != 10 || c == 10;
+
+// ESEMPIO WriteLine:
+Console.WriteLine(b); // Sarà true
+
+// ESEMPIO SWITCH:
+switch (b)
+{
+    case true:
+        Console.WriteLine("True!"); //<-- Uscirà true
+        break;
+    case false:
+        Console.WriteLine("False!");
+        break;
+}
+
+// ESEMPIO WHILE
+int i = 0;
+while (i < 10)
+{
+    if(i % 2 != 0)
+    {
+        i++;
+        continue;
+    }
+    Console.WriteLine(i); //Printerà solo numeri pari fino all'8
+    i++;
+}
+
+string valori = "1,2,3,4,5";
+// Separatore, in questo caso da ogni virgola
+string[] result = valori.Split(','); 
+// Printerà il valore 4, che in questo caso è 5
+Console.WriteLine(result[4]); 
+
+// Stessa cosa ma con il nome pluto
+string[] resultNome = nome.Split('u');
+// Printerà "pl" essendo diviso dalla u, ma se metto 1 printerà "to"
+Console.WriteLine(resultNome[0]); 
+
+// Sempre un metodo per creare un array ma con "ToArray()"
+string caratteri = "a b c d e";
+// Sostituisce il primo valore (che sarebbe lo spazio) con il secondo valore (che sarebbe nulla, quindi cancellando gli spazi)
+string carPulizia = caratteri.Replace(" ", ""); 
+char[] caratteriArray = carPulizia.ToArray();
+// Viene divisa automanticamente ogni lettera, in questo caso printerà "c"
+Console.WriteLine(caratteriArray[2]);
+
+// ESEMPIO FOR
+// Printerà ogni lettera di "caratteriArray", assegnandogli un valore numerico
+for (int j = 0; j < caratteriArray.Length; j++)
+{
+    Console.WriteLine("Il valore di [" + j + "] è: " + caratteriArray[j]);
+}
+
+// ESEMPIO FOREACH
+foreach (string item in result)
+{
+    Console.WriteLine("Il valore dell'elemento corrente è: " + item);
+}
+
+// Metodo per creare una serie di stringhe
+StringBuilder myBuilder = new StringBuilder(); //Dichiarazione di una variabile
+myBuilder.Append(nome);
+myBuilder.Append(" - ");
+myBuilder.Append(cognome);
+Console.WriteLine(myBuilder.ToString());
+
+// typeof ci dice il corso della determinata classe, in questo caso creata da me
+Console.WriteLine(typeof(Persona));
+
+// Creo l'oggetto e gli assegno dei dati
+Persona persona1 = new Persona("Gianmarco", "Friggitelli", new DateOnly(1994, 01, 23));
+// In questo caso lo sto assegnando manualmente
+persona1.CF = "AF3FS3SCNG54HI5";
+persona1.Email = "gianmarchini82@gmail.com";
+
+// Adesso printo tutto, con un metodo scritto in person.cs
+persona1.DisplayBasicInfo();
+
+
---
+++ CSharpCorso/person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCorso
+{
+    internal class Persona
+    {
+        internal Persona(string Nome, string Cognome, DateOnly DataDiNascita)
+        {
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+            this.DataDiNascita = DataDiNascita;
+        }
+
+        internal string Nome { get; set; } // Proprietà, permettere di leggere e scrivere (get e set)
+        internal string Cognome { get; set; }
+        internal string CF { get; set; }
+        internal string Email { get; set; }
+        internal string Indirizzo { get; set; }
+        internal DateOnly DataDiNascita { get; set; }
+
+        // Useremo questa funzione per printare le 3 info principali
+        public void DisplayBasicInfo()
+        {
+            Console.WriteLine("Nome: " + this.Nome + "\n" + "Cognome: " + this.Cognome + "\n" + "Data di Nascita: " + this.DataDiNascita);
+        }
+    }
+}
